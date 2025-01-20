98224d837468a46c0091297c1c8701a115cbbaf4
SHRIVARDHAN PATIL <160032781+patilshri2019@users.noreply.github.com>
2025-01-20 16:43:18 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,13 @@
+public class circle
+{
+public static void main(String[] args)
+{
+double radius = 0.5;
+double area = 3.14 * radius * radius;
+double circumference = 2 * Math.PI * radius;
+System.out.println("Radis of circle: " + radius);
+System.out.println("area of circle: " + area);
+System.out.println("circumference of circle: " +  circumference);
+
+}
+}
