9c33154ed981423931b2f7d9069f49e4895e23a5
SHRIVARDHAN PATIL <160032781+patilshri2019@users.noreply.github.com>
2025-01-20 16:43:44 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class program 
+{
+public static void main(String[] args)
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("1st value");
+	double a = sc.nextDouble();
+
+
+	System.out.println("2nd value");
+	double b = sc.nextDouble();
+
+	System.out.println(Math.max(a,b));
+	}
+}
