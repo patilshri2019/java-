0c10fa3bbbf3e090f03995caa5e34b2d358321d8
SHRIVARDHAN PATIL <160032781+patilshri2019@users.noreply.github.com>
2025-01-23 19:32:32 +0530

    Add files via upload

---
+++ calculeter.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class calculeter {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number first");
+        int a = sc.nextInt();
+        System.out.println("Enter a 2nd number");
+        int b = sc.nextInt();
+
+        System.out.println("addition:" +  a+b);
+        
+        System.out.println("Div:"+ a/b);
+        System.out.println("mul:" + a*b);
+
+
+
+
+    }
+}
